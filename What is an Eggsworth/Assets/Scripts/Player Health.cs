349d2c64298bcb3efbd598840d5faaccc556a6be
Jacob-Chamberlin <112023447+Jacob-Chamberlin@users.noreply.github.com>
2025-10-01 13:26:07 -0600

    quick changes

--- What is an Eggsworth/Assets/Scripts/Player Health.cs
+++ What is an Eggsworth/Assets/Scripts/Player Health.cs
@@ -1,16 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int hp = 5;
+    public bool canTakeDmg = true;
+    public float damageCD = 2;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hp > 0)
+        {
+
+        }
+    }
+    public void OnCollisionEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Hazard"))
+        {
+            
+        }
+    }
+    private IEnumerator TakeDamage(int amount)
+    {
+        if (canTakeDmg)
+        {
+            canTakeDmg = false;
+            hp = hp - amount;
+            yield return new WaitForSeconds(damageCD);
+            canTakeDmg = true;
+        }
     }
 }
